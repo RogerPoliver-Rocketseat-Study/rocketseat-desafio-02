21c12c060ce5c10b76fb6922281b84bc5d78643f
Roger Oliveira <rogerpolvr@gmail.com>
2020-06-18 20:30:59 -0300

    Implementando a rota DELETE/repositories/:id - TESTS: 6 OF 9

--- src/app.js
+++ src/app.js
@@ -33,7 +33,8 @@ app.put("/repositories/:id", (request, response) => {
     const { id } = request.params;
     const { title, url, techs } = request.body;
 
-    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    const repositoryIndex = repositories
+        .findIndex(repository => repository.id === id);
 
     if (repositoryIndex < 0) {
         return response.status(400).json({ error: "Repository not found." })
@@ -52,7 +53,17 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-    // TODO
+    const { id } = request.params;
+    const repositoryIndex = repositories
+        .findIndex(repository => repository.id === id);
+
+    if (repositoryIndex < 0) {
+        return response.status(400).json({ error: "Repository not found." })
+    }
+
+    repositories.splice(repositoryIndex, 1);
+
+    return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
